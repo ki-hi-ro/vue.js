4f2d9e6fbf564eab2ce8f2926bcccdb4c7750935
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-12 06:16:59 +0900

    1000ミリ秒ごとにカウントされるカウンターを作成した

--- assets/js/my-vue.js
+++ assets/js/my-vue.js
@@ -7,3 +7,18 @@ const HelloVue = {
 }
 
 Vue.createApp(HelloVue).mount('#hello-vue')
+
+const Counter = {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+};
+
+Vue.createApp(Counter).mount("#counter");
