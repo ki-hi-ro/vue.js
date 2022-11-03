1460c6a399c97f02341f3dc6d12fdab767232ddb
khiro <hiroki.hiroki@icloud.com>
2022-11-03 20:53:30 +0900

    カウントアップ

--- assets/js/my-vue.js
+++ assets/js/my-vue.js
@@ -1,24 +1,10 @@
 const HelloVue = {
   data() {
     return {
-      message: 'Hello Vue'
-    }
-  }
-}
-
-Vue.createApp(HelloVue).mount('#hello-vue')
-
-const Counter = {
-  data() {
-    return {
-      counter: 0,
+      count: 0,
     };
   },
-  mounted() {
-    setInterval(() => {
-      this.counter++
-    }, 1000)
-  }
 };
 
-Vue.createApp(Counter).mount("#counter");
+Vue.createApp(HelloVue).mount("#app");
+
